84a82012d52e050d5cb8d27ea6ffddaa71c4cad3
soleilvermeil <julien.leuenberger@outlook.com>
2026-05-07 15:55:12 +0200

    fix: remove revalidate path for reset page

--- app/reset/page.tsx
+++ app/reset/page.tsx
@@ -1,4 +1,3 @@
-import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { resetDb } from "@/lib/db/reset";
 
@@ -6,6 +5,5 @@ export const dynamic = "force-dynamic";
 
 export default async function ResetPage() {
   resetDb();
-  revalidatePath("/", "layout");
   redirect("/");
 }
